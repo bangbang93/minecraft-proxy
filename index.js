53716c5bd512be40a4f74bc0b5f5b19c20cf86c2
bangbang93 <bangbang93@163.com>
2015-01-11 01:59:44 +0800

    添加多服务器的支持

--- index.js
+++ index.js
@@ -5,22 +5,21 @@ var net = require('net');
 var crypto = require('crypto');
 var protocol = require('./protocol');
 
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
 var connections = {};
 
 var server = net.createServer();
 
 server.on('connection', function (client){
-    var mc = net.connect({
-        port: 25565
-    });
+
     var buffer = new Buffer(0);
     var state = 'handshaking';
     var handshake;
     var uuid;
     client.on('data', function (data){
         buffer = Buffer.concat([buffer, data]);
-        console.log(buffer);
-        console.log(state);
         while (true){
             var result = protocol.parsePacket(buffer, state, true, {
                 set_protocol: 1,
@@ -37,23 +36,57 @@ server.on('connection', function (client){
             console.log(state);
             if (state == 'handshaking'){
                 if (result.results['nextState'] == 1){
-                    mc.write(buffer);
-                    makePipe(client, mc);
+                    var server = config['servers'][result.results['serverHost']];
+                    if (!server){
+                        client.close();
+                    } else {
+                        var host = server.host;
+                        var port = server.port;
+                        var mc = net.connect({
+                            host: host,
+                            port: port
+                        });
+                        (function (mc, buffer){
+                            mc.on('connect', function (){
+                                mc.write(buffer);
+                                client.removeAllListeners('data');
+                                makePipe(client, mc);
+                            })
+                        })(mc, buffer);
+                    }
                 } else {
                     handshake = result.results;
                     state = 'login';
                 }
             } else if (state = 'login'){
-                var packet = result.results;
-                var usernameMD5 = md5(packet['username']);
-                uuid = usernameMD5.substr(0,8) + '-' + usernameMD5.substr(8,4) + '-' + usernameMD5.substr(12,4) + '-' + usernameMD5.substr(16,4) + '-' + usernameMD5.substr(20,12);
-                handshake['serverHost'] += '\0' + client.remoteAddress + '\0' + uuid;
-                var handshakePacket = protocol.createPacketBuffer(0x00, 'handshaking', handshake, false);
-                var loginPacket = protocol.createPacketBuffer(0x00, 'login', packet, false);
-                var newBuffer = Buffer.concat([handshakePacket, loginPacket]);
-                mc.write(newBuffer);
-                client.removeAllListeners('data');
-                makePipe(client, mc);
+                server = config['servers'][handshake['serverHost']];
+                if (!server){
+                    client.write(protocol.createPacketBuffer(0x00, 'login', {
+                        reason: '服务器不存在'
+                    }, true));
+                } else {
+                    host = server.host;
+                    port = server.port;
+                    mc = net.connect({
+                        host: host,
+                        port: port
+                    });
+                    (function (mc, result){
+                        mc.on('connect', function (){
+                            var packet = result.results;
+                            var usernameMD5 = md5(packet['username']);
+                            uuid = usernameMD5.substr(0,8) + '-' + usernameMD5.substr(8,4) + '-' + usernameMD5.substr(12,4) + '-' + usernameMD5.substr(16,4) + '-' + usernameMD5.substr(20,12);
+                            handshake['serverHost'] += '\0' + client.remoteAddress + '\0' + uuid;
+                            var handshakePacket = protocol.createPacketBuffer(0x00, 'handshaking', handshake, false);
+                            var loginPacket = protocol.createPacketBuffer(0x00, 'login', packet, false);
+                            var newBuffer = Buffer.concat([handshakePacket, loginPacket]);
+                            mc.write(newBuffer);
+                            client.removeAllListeners('data');
+                            makePipe(client, mc);
+                        })
+                    })(mc, result);
+                }
+
             }
             buffer = buffer.slice(result.size);
         }
@@ -73,4 +106,4 @@ function md5 (str) {
     return str;
 }
 
-server.listen(25566);
+server.listen(config.port, config.host);
