433ebb0edd8a8d470be0742cfd4ded44ba0d33eb
bangbang93 <bangbang93@163.com>
2015-02-09 21:39:07 +0800

    代码中移除测试插件

--- plugins/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by bangbang on 15/02/09.
- */
-module.exports = function (Proxy){
-    console.log('测试插件加载成功');
-    Proxy.on('ready', function (){
-        console.log('准备就绪，该信息由插件输出');
-    })
-};
