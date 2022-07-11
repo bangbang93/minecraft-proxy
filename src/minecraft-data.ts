cd741628e1e03c4e09114de23f23980c2bfbc682
bangbang93 <bangbang93@163.com>
2022-07-11 18:12:32 +0800

    protocol version不认识的时候给个warning

--- src/minecraft-data.ts
+++ src/minecraft-data.ts
@@ -1,10 +1,17 @@
+import {createLogger} from 'bunyan'
 import {postNettyVersionsByProtocolVersion, preNettyVersionsByProtocolVersion} from 'minecraft-data'
 import {Service} from 'typedi'
 
 @Service()
 export class MinecraftData {
+  private logger = createLogger({name: 'minecraft-data'})
+
   public protocolVersionToMcVersion(protocolVersion: number): string {
-    return preNettyVersionsByProtocolVersion.pc[protocolVersion]?.minecraftVersion
+    const res = preNettyVersionsByProtocolVersion.pc[protocolVersion]?.minecraftVersion
         ?? postNettyVersionsByProtocolVersion.pc[protocolVersion]?.minecraftVersion
+    if (!res) {
+      this.logger.warn('unknown protocol version: %d', protocolVersion)
+    }
+    return null
   }
 }
